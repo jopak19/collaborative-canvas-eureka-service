cc82b098ae69ae622c4af945e05a5106a753058d
jopak19 <jpaulogomessiqueira@gmail.com>
2023-06-27 21:59:56 -0300

    configura eureka server

---
+++ eureka-service/src/test/java/br/ufrn/dimap/collaborativecanvas/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufrn.dimap.collaborativecanvas.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
